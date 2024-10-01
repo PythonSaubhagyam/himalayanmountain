3145a5f56c1855acc84e057414c7bb12061b6226
dixi1365 <dixi@saubhagyam.org>
2024-10-01 18:54:13 +0530

    button disable during send

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -14,21 +14,18 @@ import {
   Button,
   useToast,
   Box,
-  useBreakpointValue,
-  Image
+  Image, useBreakpointValue
 } from "@chakra-ui/react";
 import client from "../setup/axiosClient";
 import { AsyncSelect } from "chakra-react-select";
 import checkLogin from "../utils/checkLogin";
 import BreadCrumbCom from "../components/BreadCrumbCom";
-
 import { useLocation } from "react-router-dom";
 
 export default function ContactUs() {
   let { search } = useLocation();
-    const searchParams = new URLSearchParams(search);
-   const IsMobileView = searchParams.get("mobile") ?? "false";
-
+  const searchParams = new URLSearchParams(search);
+  const IsMobileView = searchParams.get("mobile") ?? "false";
   const initialFormData = Object.freeze({
     company: "",
     name: "",
@@ -39,12 +36,14 @@ export default function ContactUs() {
     inquiry_description: "",
     age_group: "00 to 06",
   });
-  const width = useBreakpointValue({md:"340px",base:"300px"})
+
   const [formData, setFormData] = useState(initialFormData);
+  const [loading , setLoading] = useState(false)
   const [countries, setCountries] = useState([]);
   const [callingCode, setCallingCode] = useState("");
   const toast = useToast();
   const loginInfo = checkLogin();
+  const width = useBreakpointValue({md:"340px",base:"300px"})
   useEffect(() => {
     getCountries(); // eslint-disable-next-line
   }, []);
@@ -69,6 +68,7 @@ export default function ContactUs() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true)
     try {
       formData.country = formData.country.value;
       const response = await client.post("/inquiries/", {
@@ -76,6 +76,7 @@ export default function ContactUs() {
         phone: "+" + callingCode + formData.phone,
       });
       if (response.data.status === true) {
+        setLoading(false)
         toast({
           title: response.data.message,
           status: "success",
@@ -85,6 +86,7 @@ export default function ContactUs() {
         });
         setFormData(initialFormData);
       } else {
+        setLoading(false)
         toast({
           title: response.data.message,
           status: "error",
@@ -94,6 +96,7 @@ export default function ContactUs() {
         });
       }
     } catch (error) {
+      setLoading(false)
       toast({
         title: error.response.data.message,
         status: "error",
@@ -127,13 +130,11 @@ export default function ContactUs() {
   };
   return (
     <>
-   
-   {IsMobileView !== "true" && <Navbar />}
-
-
+       {IsMobileView !== "true" && <Navbar />}
       <Container maxW="container.xl">
         <BreadCrumbCom second={"Contact Us"} secondUrl={"/contact-us"} />
       </Container>
+     
       <Container maxW={"container.xl"} py={1} px={0} position="relative">
         <Image src="https://forntend-bucket.s3.ap-south-1.amazonaws.com/sose/images/organic-living/contact.jpg" />
 
@@ -174,7 +175,7 @@ export default function ContactUs() {
           <FormControl
             as={Flex}
             direction={{ base: "column", md: "row" }}
-            align={{md:"center",base:"flex-start"}}
+             align={{md:"center",base:"start"}}
             isRequired
           >
             <FormLabel
@@ -202,7 +203,7 @@ export default function ContactUs() {
           <FormControl
             as={Flex}
             direction={{ base: "column", md: "row" }}
-            align={{md:"center",base:"flex-start"}}
+            align={{md:"center",base:"start"}}
             isRequired
             mt="5"
           >
@@ -231,7 +232,7 @@ export default function ContactUs() {
           <FormControl
             as={Flex}
             direction={{ base: "column", md: "row" }}
-            align={{md:"center",base:"flex-start"}}
+             align={{md:"center",base:"start"}}
             isRequired
             mt="5"
           >
@@ -272,7 +273,7 @@ export default function ContactUs() {
                 inputContainer: (provided) => ({
                   ...provided,
                   maxWidth:width,
-                  minWidth: width
+                  minWidth:width,
                 }),
               }}
               variant={"outline"}
@@ -295,7 +296,7 @@ export default function ContactUs() {
             <FormControl
               as={Flex}
               direction={{ base: "column", md: "row" }}
-              align={{md:"center",base:"flex-start"}}
+               align={{md:"center",base:"start"}}
               isRequired
               mt="5"
             >
@@ -331,7 +332,7 @@ export default function ContactUs() {
           <FormControl
             as={Flex}
             direction={{ base: "column", md: "row" }}
-            align={{md:"center",base:"flex-start"}}
+             align={{md:"center",base:"start"}}
             isRequired
             mt="5"
           >
@@ -360,7 +361,7 @@ export default function ContactUs() {
           <FormControl
             as={Flex}
             direction={{ base: "column", md: "row" }}
-            align={{md:"center",base:"flex-start"}}
+             align={{md:"center",base:"start"}}
             isRequired
             mt="5"
           >
@@ -389,7 +390,7 @@ export default function ContactUs() {
           <FormControl
             as={Flex}
             direction={{ base: "column", md: "row" }}
-            align={{md:"center",base:"flex-start"}}
+             align={{md:"center",base:"start"}}
             isRequired
             mt="5"
             mb={"5"}
@@ -418,7 +419,7 @@ export default function ContactUs() {
             />
           </FormControl>
           <Container maxW={"lg"} p="0">
-            <Button type="submit" colorScheme={"brand"}>
+            <Button type="submit" isLoading={loading} loadingText={"Sending"} colorScheme={"brand"}>
               Send
             </Button>
           </Container>
@@ -426,7 +427,6 @@ export default function ContactUs() {
 
       </Container>
       {IsMobileView !== "true" && <Footer />}
-
     </>
   );
 }
